781469af1f68fdad355a69594b612a144a58d8b5
강주원 <wizuded@naver.com>
2024-03-07 17:43:56 +0900

    feat: spring initializr 를 이용하여 초기 개발환경 구축

---
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/SorimaeulApplication.java
@@ -0,0 +1,13 @@
+package com.usagi.sorimaeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorimaeulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorimaeulApplication.class, args);
+	}
+
+}
---
+++ sorimaeul/src/test/java/com/usagi/sorimaeul/SorimaeulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usagi.sorimaeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorimaeulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
