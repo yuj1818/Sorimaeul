55740433663e8b7f2a63f15015d44ecc09732306
강주원 <wizuded@naver.com>
2024-03-11 17:38:18 +0900

    feat: User 엔티티 작성

--- sorimaeul/src/main/java/com/usagi/sorimaeul/entity/User.java
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/entity/User.java
@@ -1,4 +1,32 @@
 package com.usagi.sorimaeul.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_tb")
 public class User {
+
+    @Id
+    @Column(name = "user_code")
+    private long userCode;
+
+    @Column(name = "nickname")
+    private String nickname;
+
+    @Column(name = "profile_image")
+    private String profileImage;
+
+    @Column(name = "social_type")
+    private String socialType;
+
+    @Column(name = "join_date")
+    private LocalDateTime joinDate;
+
+    @Column(name = "learn_count")
+    private int learnCount;
 }
