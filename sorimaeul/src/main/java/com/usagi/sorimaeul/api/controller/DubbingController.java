632649e25d19e89545dc4584f4135916ecf4a3c9
junhyung <wnsgud0895@gmail.com>
2024-03-12 17:35:32 +0900

    wip: 더빙 관련 작업중 파일 업로드

---
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/api/controller/DubbingController.java
@@ -0,0 +1,11 @@
+package com.usagi.sorimaeul.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/dub")
+public class DubbingController {
+}
