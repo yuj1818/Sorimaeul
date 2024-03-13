af81bd08f2b282e2d1a89e4ad58b96e42eec653a
강주원 <wizuded@naver.com>
2024-03-13 15:38:46 +0900

    refactor: 빌드를 위한 임시 주석 처리

--- sorimaeul/src/main/java/com/usagi/sorimaeul/repository/VoiceModelRepository.java
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/repository/VoiceModelRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface VoiceModelRepository extends JpaRepository<VoiceModel, Integer> {
 
-    List<VoiceModel> findByUserCodeAndVideoCode(long userCode, Integer videoCode);
+//    List<VoiceModel> findByUser_UserCodeAndVideoSource_VideoSourceCode(long userCode, Integer videoSourceCode);
 
 }
