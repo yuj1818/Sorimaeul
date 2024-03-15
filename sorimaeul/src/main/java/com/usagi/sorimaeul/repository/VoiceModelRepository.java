fd22fa5ba940572d5c0965af14f98fc40ad7a6b4
강주원 <wizuded@naver.com>
2024-03-15 16:33:19 +0900

    refactor: 안 쓰는 코드 삭제

--- sorimaeul/src/main/java/com/usagi/sorimaeul/repository/VoiceModelRepository.java
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/repository/VoiceModelRepository.java
@@ -14,9 +14,4 @@ public interface VoiceModelRepository extends JpaRepository<VoiceModel, Integer>
 
     VoiceModel findByModelCode(int modelCode);
 
-    // 기본 모델 가져오기
-//    @Query(value = "SELECT new com.usagi.sorimaeul.dto.dto.ModelInfoDto(v.modelCode, v.modelName, v.imagePath, v.recordCount, v.state) " +
-//            "FROM VoiceModel v WHERE v.state = 3 AND v.user.userCode = NULL AND v.videoSource.videoSourceCode = NULL")
-//    List<ModelInfoDto> getCommonModelList();
-
 }
