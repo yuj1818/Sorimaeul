09f1cf5683531b5b7dfa78fc9b2759fd71fa0b7e
강주원 <wizuded@naver.com>
2024-03-18 22:39:07 +0900

    feat: Cover 컨트롤러에 AI 커버 목록 조회 작성

--- sorimaeul/src/main/java/com/usagi/sorimaeul/api/controller/CoverController.java
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/api/controller/CoverController.java
@@ -1,13 +1,32 @@
 package com.usagi.sorimaeul.api.controller;
 
+import com.usagi.sorimaeul.api.service.CoverService;
+import com.usagi.sorimaeul.dto.request.ModelTableCreateRequest;
+import com.usagi.sorimaeul.dto.response.CoverListResponse;
+import com.usagi.sorimaeul.dto.response.ModelTableCreateResponse;
+import com.usagi.sorimaeul.utils.JwtTokenProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cover")
 @RequiredArgsConstructor
 @Tag(name = "Cover 컨트롤러", description = "AI 커버 관리를 위한 API")
 public class CoverController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CoverService coverService;
+
+    @Operation(summary = "AI 커버 목록 조회", description = "AI 커버 목록을 조회한다.")
+    @ApiResponse(responseCode = "200", description = "AI 커버 목록 조회 성공")
+    @GetMapping
+    public ResponseEntity<CoverListResponse> getCoverList(@RequestHeader("Authorization") String token,
+                                                          @RequestParam String target, @RequestParam String keyword, @RequestParam int page) {
+        long userCode = Long.parseLong(jwtTokenProvider.getPayload(token.substring(7)));
+        return coverService.getCoverList(userCode, target, keyword, page);
+    }
 }
