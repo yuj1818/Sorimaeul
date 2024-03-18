ac53d420cf4f5c9987646575a0f767b71cbcf590
강주원 <wizuded@naver.com>
2024-03-18 15:57:51 +0900

    feat: cover_tb 리포지토리 생성

---
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/repository/CoverRepository.java
@@ -0,0 +1,12 @@
+package com.usagi.sorimaeul.repository;
+
+import com.usagi.sorimaeul.entity.Cover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CoverRepository extends JpaRepository<Cover, Integer> {
+
+    List<Cover> findByUser_userCode(long userCode);
+
+}
