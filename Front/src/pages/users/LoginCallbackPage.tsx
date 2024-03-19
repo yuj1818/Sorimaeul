38365a519977196ce4ef41ab14f3320fd998ed40
탁윤희 <gwalgwal98@naver.com>
2024-03-19 13:57:28 +0900

    fix: signup 페이지로 이동한 뒤, cookie가 저장되지 않은 문제 해결

--- Front/src/pages/users/LoginCallbackPage.tsx
+++ Front/src/pages/users/LoginCallbackPage.tsx
@@ -4,66 +4,53 @@ import { Navigate, useLocation } from "react-router-dom";
 import API from "../../utils/axios";
 import { setCookie, getCookie, removeCookie } from "../../utils/cookie";
 
-
-
-
 // 소셜 로그인 후 redirect될 페이지 (call back)
 // 서버로부터 token(access, refresh)까지 받는 역할
 const LoginCallbackPage: React.FC = () => {
-    const [status, setStatus] = useState<number | null>(null);
-    const provider:string = useLocation().pathname.slice(16);
-    const code:string | null = new URLSearchParams(useLocation().search).get("code");
+  const [status, setStatus] = useState<number | null>(null);
+  const provider:string = useLocation().pathname.slice(16);
+  const code:string | null = new URLSearchParams(useLocation().search).get("code");
 
-    if (code) {
-        useEffect(() => {API.get(`/oauth/login/${provider}?code=${code}`)
-        .then((res) => {
-            const token = res.data.accessToken;
-            // if (getCookie("accessToken")) {
-            //     removeCookie("accessToken");
-            // }
-            
-            // 쿠키 설정 및 생성 - 보안 설정 수정 필요
-            setCookie("accessToken",`Bearer ${token}`, {
-                secure: false,  // 추후 true로 바꿔 Https 연결로만 전송 가능하게 하기 + sameSite: Strict 설정
-                httpOnly: false, // XSS 공격 방지 - JS로 쿠키 접근 x 
-                sameSite: 'none'
-            });
+  useEffect(() => {
+      if (code) {
+          API.get(`/oauth/login/${provider}?code=${code}`)
+          .then((res) => {
+              const token = res.data.accessToken;
 
-            // 로그인 
-            useEffect(() => {
-                if (token) {
-                  API.get("/user/login", {
-                    headers: {
-                      Authorization: token
-                    }
-                  })
-                  .then((res) => {
-                    setStatus(res.status);
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                  });
-                } else {
-                  console.log("토큰이 없습니다.");
-                }
-              }, [token]);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    }, [code, provider]);
+              // accessToken이 쿠키에 저장되어있으면 삭제
+              if (getCookie("accessToken")) {
+                  removeCookie("accessToken");
+              }
 
+              setCookie("accessToken",`Bearer ${token}`, { path: "/"}); // Https 적용하면 option 설정 하자!
 
-    
-    
-        return (
-            <div>
-                {status === 200 && <Navigate to={"/home"} />}
-                {status === 204 && <Navigate to={"/signup"} />}
-            </div>
-        );
-    }
+              const accessToken = getCookie("accessToken");
 
+              // 로그인 요청 보내기 
+              API.get("/user/login", {
+                  headers: {
+                      Authorization: accessToken
+                  }
+              })
+              .then((res) => {
+                  setStatus(res.status);
+              })
+              .catch((err) => {
+                  console.log(err);
+              });
+          })
+          .catch((err) => {
+              console.log(err);
+          });
+      }
+  }, [code, provider]);
+
+  return (
+      <div>
+          {status === 200 && <Navigate to={"/home"} />}
+          {status === 204 && <Navigate to={"/signup"} />}
+      </div>
+  );
 };
 
 export default LoginCallbackPage;
