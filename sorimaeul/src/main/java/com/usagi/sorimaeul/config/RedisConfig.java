695258e541403442bfbf0a4df06bc766821329ac
김정민 <kjm1994@naver.com>
2024-03-19 10:44:59 +0900

    fix: config 수정

--- sorimaeul/src/main/java/com/usagi/sorimaeul/config/RedisConfig.java
+++ sorimaeul/src/main/java/com/usagi/sorimaeul/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
@@ -17,9 +18,16 @@ public class RedisConfig {
 	@Value("${jwt.redis.port}")
 	private int redisPort;
 
+	@Value("${jwt.redis.password}")
+	private String redisPassword;
+
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisHost, redisPort);
+		RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
+		redisConfiguration.setHostName(redisHost);
+		redisConfiguration.setPort(redisPort);
+		redisConfiguration.setPassword(redisPassword);
+		return new LettuceConnectionFactory(redisConfiguration);
 	}
 
 }
