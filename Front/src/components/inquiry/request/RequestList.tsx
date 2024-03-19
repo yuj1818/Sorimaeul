e1652db4fc22df3fa302477e62067ee49e759eda
yuj1818 <po042@naver.com>
2024-03-19 15:32:16 +0900

    design: 요청 게시판 목록 조회 페이지 UI 구성

---
+++ Front/src/components/inquiry/request/RequestList.tsx
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+const Line = styled.hr<{$color?: string}>`
+  border-color: ${(props) => (props.$color ? `${props.$color}rem` : "black")};
+`
+
+const List = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: .5rem;
+
+  .title {
+    font-family: 'GmarketSansMedium';
+    font-size: 1.25rem;
+    margin-top: .25rem;
+  }
+
+  .date {
+    font-family: 'GmarketSansLight';
+    font-size: 0.75rem;
+    margin-top: .25rem;
+  }
+`
+
+function RequestList() {
+  const data = [
+    {
+      id: 1,
+      title: "타짜 영상 올려주세요",
+      createdTime: "2024-03-19"
+    },
+    {
+      id: 2,
+      title: "짱구는 못말려 영상도 올려주세요",
+      createdTime: "2024-03-19"
+    },
+    {
+      id: 3,
+      title: "요청요청",
+      createdTime: "2024-03-18"
+    },
+    {
+      id: 4,
+      title: "요청요청",
+      createdTime: "2024-03-18"
+    },
+    {
+      id: 5,
+      title: "요청요청",
+      createdTime: "2024-03-18"
+    },
+    {
+      id: 6,
+      title: "요청요청",
+      createdTime: "2024-03-17"
+    }
+  ]
+  
+  return (
+    <List>
+      <div className="flex justify-center items-center">
+        <p className="title w-3/4">제목</p>
+        <p className="title w-1/6">게시 날짜</p>
+      </div>
+      <Line />
+      {
+        data.map(el => (
+          <>
+            <div className="flex items-center justify-center">
+              <p className="title w-3/4">{el.title}</p>
+              <p className="date w-1/6">{el.createdTime}</p>
+            </div>
+            <Line $color="#A3A3A3" />
+          </>
+        ))
+      }
+    </List>
+  )
+}
+
+export default RequestList;
--- Front/src/pages/inquiry/RequestListPage.tsx
+++ Front/src/pages/inquiry/RequestListPage.tsx
@@ -2,6 +2,7 @@ import ColorLine from "../../components/inquiry/ColorLine";
 import SubTitle from "../../components/inquiry/SubTitle";
 import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/common/Button";
+import RequestList from "../../components/inquiry/request/RequestList";
 
 function RequestListPage() {
   const navigate = useNavigate();
@@ -9,14 +10,15 @@ function RequestListPage() {
   return (
     <>
       <ColorLine />
-      <div className="mx-32 w-9/12 flex flex-col gap-8 mt-12">
+      <div className="mx-auto w-9/12 flex flex-col gap-8 mt-12">
         <div className="flex gap-4 items-center">
           <SubTitle>
             <span className="lg-font">요</span>청 게시판
           </SubTitle>
           <SubTitle><span className="disabled" onClick={() => navigate('/FAQ')}>| 자주 묻는 질문 FAQ</span></SubTitle>
         </div>
-        <Button onClick={() => navigate('/request/create')} $marginLeft={0} $marginTop={0} $fontSize={0.875} $width={4.375} $height={2} $background="#28BEFF">글쓰기</Button>
+        <RequestList />
+        <Button onClick={() => navigate('/request/create')} $marginLeft={0} $marginTop={0} $width={4.375} $height={2} $background="#28BEFF">글쓰기</Button>
       </div>
     </>
   )
