4dbdb00be8e8e4783d976a947197534ddd0e23d1
yuj1818 <po042@naver.com>
2024-03-19 12:33:37 +0900

    feat: 소제목 컴포넌트 생성

---
+++ Front/src/components/inquiry/SubTitle.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const SubTitle = styled.span`
+  font-family: 'GmarketSansBold';
+  .lg-font {
+    font-size: 2.5rem;
+  }
+  font-size: 2rem;
+  color: black;
+  .disabled {
+    color: #A3A3A3;
+    cursor: pointer;
+  }
+`
+
+export default SubTitle;
