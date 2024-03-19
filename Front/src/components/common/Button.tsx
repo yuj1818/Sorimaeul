2a1c663311a2f3a2b01d0e1784a6f5a89726a8fe
yuj1818 <po042@naver.com>
2024-03-19 12:33:19 +0900

    feat: Button 공통 컴포넌트 생성

---
+++ Front/src/components/common/Button.tsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+interface buttonInterface {
+  $color?: string;
+  $fontSize?: number;
+  $height?: number;
+  $width?: number;
+  $marginTop?: number;
+  $paddingX?: number;
+  $paddingY?: number;
+  $fontFamily?: string;
+  $marginLeft?: number;
+  $background?: string;
+  $border?: string;
+  $selected?: boolean;
+}
+
+export const Button = styled.button<buttonInterface>`
+  border: ${(props) => props.$border || "none"};
+  border-radius: 5px;
+  color: ${(props) => props.color || "white"};
+  font-size: ${(props) => `${props.$fontSize || 1}rem`};
+  padding-top: ${(props) =>
+    props.$paddingY === 0 || props.$paddingY
+      ? `${props.$paddingY}rem`
+      : "0.2rem"};
+  padding-bottom: ${(props) =>
+    props.$paddingY === 0 || props.$paddingY
+      ? `${props.$paddingY}rem`
+      : "0.2rem"};
+  padding-left: ${(props) =>
+    props.$paddingX === 0 || props.$paddingX
+      ? `${props.$paddingX}rem`
+      : "0.4rem"};
+  padding-right: ${(props) =>
+    props.$paddingX === 0 || props.$paddingX
+      ? `${props.$paddingX}rem`
+      : "0.4rem"};
+  white-space: nowrap;
+  height: ${(props) => (props.$height ? `${props.$height}rem` : "auto")};
+  width: ${(props) => (props.$width ? `${props.$width}rem` : "auto")};
+  margin-top: ${(props) =>
+    props.$marginTop === 0 || props.$marginTop
+      ? `${props.$marginTop}rem`
+      : "auto"};
+  margin-left: ${(props) =>
+    props.$marginLeft === undefined ? "auto" : `${props.$marginLeft}rem`};
+  background-color: ${(props) => props.$background || "black"};
+  font-family: ${(props) => props.$fontFamily || "GmarketSansMedium"};
+  opacity: ${(props) => props.$selected === undefined ? 'none' : props.$selected ? 'none' : '0.6' };
+
+  &:hover {
+    box-shadow: 3.5px 3.5px 3.5px lightgrey;
+  }
+
+  &:disabled {
+    opacity: .6;
+    cursor: not-allowed;
+  }
+`;
