163a09360ac80b5d5be4a7db3af33a2001998bca
강주원 <wizuded@naver.com>
2024-03-19 22:36:44 +0900

    feat: script 엔티티 생성

---
+++ Back/src/main/java/com/usagi/sorimaeul/entity/Script.java
@@ -0,0 +1,25 @@
+package com.usagi.sorimaeul.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "script_tb")
+public class Script {
+
+    @Id
+    @Column(name = "script_code")
+    private int scriptCode;
+
+    @Column(name = "nickname")
+    private String script;
+
+}
