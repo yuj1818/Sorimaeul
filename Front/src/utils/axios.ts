1bacccb59dd3a0725fe8697f94460fc8b56aadc5
junhyung <wnsgud0895@gmail.com>
2024-03-19 14:48:20 +0900

    fix: 빌드테스트 tsconfig ( 미사용변수 오류 안뜨게 설정 ), axios 오류 수정

--- Front/src/utils/axios.ts
+++ Front/src/utils/axios.ts
@@ -6,11 +6,20 @@ export const URL = "http://localhost:8000/api";
 
 const API = axios.create({
     baseURL: URL,
-    withCredentials: true, 
-    headers: {
-        accessToken: await getCookie("accessToken"),
-    },
-})
-  
+    withCredentials: true,
+});
+
+// 요청 인터셉터 추가
+API.interceptors.request.use(async (config) => {
+    // accessToken을 쿠키에서 비동기적으로 가져옴
+    const accessToken = await getCookie("accessToken");
+    if (accessToken) {
+        config.headers['accessToken'] = accessToken;  // 헤더에 accessToken 추가
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
 export default API;
 
