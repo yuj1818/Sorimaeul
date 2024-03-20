b8ea68356643905c5d3c42751304f3323382d18c
강주원 <wizuded@naver.com>
2024-03-20 11:08:25 +0900

    feat: PlaylistCover 리포지토리 생성

---
+++ Back/src/main/java/com/usagi/sorimaeul/repository/PlaylistCoverRepository.java
@@ -0,0 +1,8 @@
+package com.usagi.sorimaeul.repository;
+
+import com.usagi.sorimaeul.entity.PlaylistCover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaylistCoverRepository extends JpaRepository<PlaylistCover, Integer> {
+
+}
