b4ba7ba2aaf6de415837e5051faa03516570a390
강주원 <wizuded@naver.com>
2024-03-20 10:34:33 +0900

    feat: Playlist 엔티티 생성

---
+++ Back/src/main/java/com/usagi/sorimaeul/entity/Playlist.java
@@ -0,0 +1,32 @@
+package com.usagi.sorimaeul.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "playlist_tb")
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "playlist_code")
+    private int playlistCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_code", referencedColumnName = "user_code")
+    private User user;
+
+    @Column(name = "playlist_name")
+    private String playlistName;
+
+    @Column(name = "created_time")
+    private LocalDateTime createTime;
+
+}
