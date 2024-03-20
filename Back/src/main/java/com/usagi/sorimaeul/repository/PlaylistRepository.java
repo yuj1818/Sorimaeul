25ddbb549ac9affa7e31f220edffba40520f573f
강주원 <wizuded@naver.com>
2024-03-20 10:47:33 +0900

    feat: Playlist 리포지토리 생성

---
+++ Back/src/main/java/com/usagi/sorimaeul/repository/PlaylistRepository.java
@@ -0,0 +1,10 @@
+package com.usagi.sorimaeul.repository;
+
+import com.usagi.sorimaeul.entity.Playlist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PlaylistRepository extends JpaRepository<Playlist, Integer> {
+
+}
