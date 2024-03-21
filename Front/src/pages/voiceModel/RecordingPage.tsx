e79d6973c6d57d0da9e96b71c1769905dff6bc9a
yuj1818 <po042@naver.com>
2024-03-21 14:07:14 +0900

    feat: 녹음 페이지 임시 구현

--- Front/src/pages/voiceModel/RecordingPage.tsx
+++ Front/src/pages/voiceModel/RecordingPage.tsx
@@ -1,7 +1,32 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../../stores/store";
+import { getScripts } from "../../utils/voiceModelAPI";
+import { useState, useEffect } from "react";
+
+interface Script {
+  no: number,
+  script: string
+}
 
 function RecordingPage() {
+  const recordCount = useSelector((state: RootState) => state.voiceModel.recordCount);
+
+  const [scripts, setScripts] = useState<Script[]>([]);
+
+  const getScriptData = async () => {
+    const scripts = await getScripts();
+    setScripts(scripts);
+  };
+
+  useEffect(() => {
+    getScriptData();
+  }, [])
+
   return (
-    <div></div>
+    <div>
+      <p>이것은 {recordCount + 1}번째 문장</p>
+
+    </div>
   )
 }
 
