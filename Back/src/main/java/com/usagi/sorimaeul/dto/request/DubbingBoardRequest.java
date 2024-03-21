da7011e0dc683a5574571b6adaf647bf104c5431
junhyung <wnsgud0895@gmail.com>
2024-03-21 17:52:22 +0900

    wip: 더빙 게시글 등록 기능 작업중

---
+++ Back/src/main/java/com/usagi/sorimaeul/dto/request/DubbingBoardRequest.java
@@ -0,0 +1,11 @@
+package com.usagi.sorimaeul.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class DubbingBoardRequest {
+
+    private String dubName;
+    private String dubDetail;
+    private int sourceCode;
+}
