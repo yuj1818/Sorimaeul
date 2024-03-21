281cf5320188ddd95ba6989d6682516f59b77299
yuj1818 <po042@naver.com>
2024-03-21 11:51:20 +0900

    feat: 현재 학습 진행 상태에 따라 UI 변경

--- Front/src/components/voiceModel/beforeTrain/SelfRecordMethod.tsx
+++ Front/src/components/voiceModel/beforeTrain/SelfRecordMethod.tsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../stores/store";
 
 const Container = styled.div`
   width: 100%;
@@ -24,6 +26,14 @@ const Container = styled.div`
 `
 
 function SelfRecordMethod() {
+  const modelInfo = useSelector((state: RootState) => state.voiceModel);
+
+  const startRecord = () => {
+    if (modelInfo.learnState === 0) {
+      
+    }
+  }
+
   return (
     <Container>
       <div className="guide">
@@ -31,8 +41,15 @@ function SelfRecordMethod() {
         <p>진행률이 80% 이상이어야 학습 진행이 가능합니다.</p>
         <p>(최소 30분 이상의 시간이 소요되며, 중간 저장이 가능합니다.)</p>
       </div>
-      <div className="record-controller">
-        <p className="start">녹음 시작하기</p>
+      <div onClick={startRecord} className="record-controller">
+        {
+          modelInfo.recordCount > 0 ?
+          <div>
+            이미 녹음 진행 중
+          </div>
+          :
+         <p className="start">녹음 시작하기</p>
+        }
       </div>
     </Container>
   )
