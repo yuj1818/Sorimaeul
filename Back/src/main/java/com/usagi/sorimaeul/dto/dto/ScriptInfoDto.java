1808c0e7a8c397ff55cde1926a5852fd910435e1
강주원 <wizuded@naver.com>
2024-03-22 17:42:22 +0900

    fix: 스크립트 조회 Getter 안 붙어 있는 문제 해결

--- Back/src/main/java/com/usagi/sorimaeul/dto/dto/ScriptInfoDto.java
+++ Back/src/main/java/com/usagi/sorimaeul/dto/dto/ScriptInfoDto.java
@@ -1,8 +1,10 @@
 package com.usagi.sorimaeul.dto.dto;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class ScriptInfoDto {
 
     private int no;
--- Back/src/main/java/com/usagi/sorimaeul/dto/response/GetScriptResponse.java
+++ Back/src/main/java/com/usagi/sorimaeul/dto/response/GetScriptResponse.java
@@ -2,10 +2,12 @@ package com.usagi.sorimaeul.dto.response;
 
 import com.usagi.sorimaeul.dto.dto.ScriptInfoDto;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.util.List;
 
 @Builder
+@Getter
 public class GetScriptResponse {
 
     private List<ScriptInfoDto> scripts;
