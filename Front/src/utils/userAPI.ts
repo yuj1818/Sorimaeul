c20d3f119213a4432fb5ddd86c3cc8f455981198
탁윤희 <gwalgwal98@naver.com>
2024-03-23 10:04:47 +0900

    style: userAPI 코드 formatting

--- Front/src/utils/userAPI.ts
+++ Front/src/utils/userAPI.ts
@@ -7,24 +7,23 @@ export const checkLogin = () => {
   let isUserLoggedIn = false;
   if (access && refresh) {
     isUserLoggedIn = true
-  } 
+  }
   return isUserLoggedIn;
 }
 
-export const checkNickname = ( nickname: string ) => {
+export const checkNickname = (nickname: string) => {
   return API.get(`user/nickname/${nickname}`)
-  .then((res) => {
-    return res.data.result;
-  })
-  .catch((err) => {
-    return err;
-  });
+    .then((res) => {
+      return res.data.result;
+    })
+    .catch((err) => {
+      return err;
+    });
 }
 
-export const signUp = async ( nickname: string, profileImage: string ) => {
+export const signUp = (nickname: string, profileImage: string) => {
   try {
-    const res = await API.post("user/signup", { nickname, profileImage });
-    console.log("회원 가입 성공", res.data);
+    return API.post("user/signup", { nickname, profileImage });
   } catch (err) {
     console.error("회원 가입 실패", err);
   }
@@ -34,13 +33,13 @@ export const logout = () => {
   const access = getCookie("accessToken");
   const refresh = getCookie("refreshToken");
   const accessToken = access ? access.replace(/Bearer\s/, "") : undefined; // 접두사와 그 뒤의 공백 제거 -> base64 decoding error 해결
-  const refreshToken = refresh ? refresh .replace(/Bearer\s/, "") : undefined; 
+  const refreshToken = refresh ? refresh.replace(/Bearer\s/, "") : undefined;
   return API.get("oauth/logout", { params: { accessToken, refreshToken } })
-  .then(() => {
-    removeCookie("accessToken", { path: "/" });
-    removeCookie("refreshToken", { path: "/" });
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+    .then(() => {
+      removeCookie("accessToken", { path: "/" });
+      removeCookie("refreshToken", { path: "/" });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 }
