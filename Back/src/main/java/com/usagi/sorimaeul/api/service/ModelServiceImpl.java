138d6bfc5290f20c0e9552c44268420e4c82e146
강주원 <wizuded@naver.com>
2024-03-23 21:08:53 +0900

    refactor : 음성 녹음 파일 업로드 작성자가 아닌 경우 BadRequest 반환

--- Back/src/main/java/com/usagi/sorimaeul/api/service/ModelServiceImpl.java
+++ Back/src/main/java/com/usagi/sorimaeul/api/service/ModelServiceImpl.java
@@ -73,6 +73,10 @@ public class ModelServiceImpl implements ModelService {
         if (user == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
+        VoiceModel voiceModel = voiceModelRepository.findByModelCode(modelCode);
+        // 모델 소유자와 클라이언트가 일치하지 않으면 BAD_REQUEST 반환
+        if (voiceModel.getUser() != user)
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         // 모델 학습 가능 횟수 검사
         if (user.getLearnCount() < 1) return ResponseEntity.badRequest().body("모델 학습 가능 횟수가 부족합니다. 상점 페이지에서 구매후 다시 시도해주세요.");
         // 파일 업로드 확인
@@ -81,7 +85,7 @@ public class ModelServiceImpl implements ModelService {
         }
         // 폴더 경로 설정
         String folderPath = BASE_PATH + "user_" + userCode + "/model_" + modelCode + "/record/";
-        VoiceModel voiceModel = voiceModelRepository.findByModelCode(modelCode);
+        
         try {
             // 폴더 생성
             createFolder(folderPath);
