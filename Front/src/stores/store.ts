0fa781b7098ee12721be5661e54342eec2c4f1f6
yuj1818 <po042@naver.com>
2024-03-25 12:33:06 +0900

    feat: common store 설정

--- Front/src/stores/store.ts
+++ Front/src/stores/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice, { UserState } from "./user";
 import voiceModelSlice, { VoiceModelState } from "./voiceModel";
+import commonSlice, { CommonState } from "./common";
 
 export interface RootState {
   user: UserState;
   voiceModel: VoiceModelState;
+  common: CommonState;
 }
 
 const store = configureStore({
   reducer: {
     user: userSlice,
-    voiceModel: voiceModelSlice
+    voiceModel: voiceModelSlice,
+    common: commonSlice
   }
 })
 
