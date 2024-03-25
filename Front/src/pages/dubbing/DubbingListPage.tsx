781f94f31794b52c9cb1dec16c16993590c099d4
yuj1818 <po042@naver.com>
2024-03-26 02:19:03 +0900

    design: 더빙 원본 영상 목록 조회 페이지 UI 구성

--- Front/src/pages/dubbing/DubbingListPage.tsx
+++ Front/src/pages/dubbing/DubbingListPage.tsx
@@ -1,7 +1,43 @@
 import { useEffect, useState } from "react";
 import { getSourceVideoList } from "../../utils/dubbingAPI";
+import styled from "styled-components";
+import DubbingSourceCard from "../../components/dubbing/contentList/DubbingSourceCard";
 
-interface VideoData {
+const ColorBlock = styled.div`
+  width: 100%;
+  height: 11rem;
+  background: linear-gradient(90deg, rgba(253, 255, 0, 0.7) 0%, rgba(99, 218, 255, 0.7) 100%), #26BA28;
+  display: flex;
+  align-items: center;
+  margin-bottom: 2rem;
+
+  .description {
+    font-size: 1.5rem;
+    color: white;
+    margin-bottom: 0.5rem;
+  }
+`
+
+const Title = styled.h1`
+  font-family: "ClimateCrisisKRVF";
+  font-size: 4rem;
+  color: rgba(255, 255, 255, 0.5);
+  -webkit-text-stroke: 1px white;
+`
+
+const ContentList = styled.div`
+  display: flex;
+  width: 80%;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0 auto;
+
+  & > div:last-child {
+    margin-right: auto;
+  }
+`
+
+export interface VideoData {
   videoSourceCode: number;
   sourceName: string;
   thumbnailPath: string;
@@ -17,7 +53,7 @@ function DubbingListPage() {
 
   const getSourceVideos = async () => {
     const res = await getSourceVideoList(page);
-    setVideoList(res);
+    setVideoList(res.videoSources);
   }
 
   useEffect(() => {
@@ -25,7 +61,22 @@ function DubbingListPage() {
   }, [page])
 
   return (
-    <div>더빙 원본 컨텐츠 리스트</div>
+    <>
+      <ColorBlock>
+        <div className="flex ml-32 items-end gap-4">  
+          <Title>더빙 학원</Title>
+          <p className="description">영상을 선택한 후, 직접 더빙해보세요!</p>
+        </div>
+      </ColorBlock>
+      <ContentList>
+        {
+          videoList &&
+          videoList.map((data) => (
+            <DubbingSourceCard key={data.videoSourceCode} data={data} />
+          ))
+        }
+      </ContentList>
+    </>
   )
 }
 
