bf3b43ff8f97d560f33a0593deb38dd4ea822740
yuj1818 <po042@naver.com>
2024-03-26 01:26:20 +0900

    feat: 더빙 원본 영상 목록 조회 기능 구현

--- Front/src/pages/dubbing/DubbingListPage.tsx
+++ Front/src/pages/dubbing/DubbingListPage.tsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react";
+import { getSourceVideoList } from "../../utils/dubbingAPI";
+
+interface VideoData {
+  videoSourceCode: number;
+  sourceName: string;
+  thumbnailPath: string;
+  videoPlaytime: string;
+  createdTime: string;
+  storagePath: string;
+  sourceDetail: string;
+}
+
 function DubbingListPage() {
+  const [page, setPage] = useState(1);
+  const [videoList, setVideoList] = useState<VideoData[]>([]);
+
+  const getSourceVideos = async () => {
+    const res = await getSourceVideoList(page);
+    setVideoList(res);
+  }
+
+  useEffect(() => {
+    getSourceVideos();
+  }, [page])
+
   return (
     <div>더빙 원본 컨텐츠 리스트</div>
   )
