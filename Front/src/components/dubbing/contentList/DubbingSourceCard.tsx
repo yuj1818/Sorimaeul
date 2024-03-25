7497e67e6a4861baae1bf059d5ceba86e9272893
yuj1818 <po042@naver.com>
2024-03-26 02:17:46 +0900

    design: 더빙 원본 영상 카드 UI 수정

---
+++ Front/src/components/dubbing/contentList/DubbingSourceCard.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { VideoData } from "../../../pages/dubbing/DubbingListPage";
+
+const Container = styled.div`
+  flex: 0 0 21.3%;
+  box-sizing: border-box;
+  margin: 1.4%;
+  height: 8.625rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .img-box {
+    position: relative;
+    height: 88%;
+    .img {
+      height: 100%;
+      width: 100%;
+    }
+    .playtime {
+      position: absolute;
+      right: 0.3rem;
+      bottom: 0.3rem;
+      background: rgba(0, 0, 0, 0.8);
+      border-radius: 2px;
+      font-size: 0.4rem;
+      width: 14.5%;
+      height: 11.5%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: white;
+    }
+  }
+`
+
+const DubbingSourceCard: React.FC<{data: VideoData}> = ({data}) => {
+  return (
+    <Container>
+      <div className="img-box">
+        <img className="img" src={data.thumbnailPath} alt="thumbnail" />
+        <div className="playtime">{data.videoPlaytime}</div>
+      </div>
+      <p className="title">{data.sourceName}</p>
+    </Container>
+  )
+}
+
+export default DubbingSourceCard;
