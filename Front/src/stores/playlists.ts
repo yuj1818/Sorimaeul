e3331199535a294d238a3bb4bc29058540fbcc84
탁윤희 <gwalgwal98@naver.com>
2024-03-26 00:13:40 +0900

    feat: playlist state들 redux store 등록

---
+++ Front/src/stores/playlists.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction  } from "@reduxjs/toolkit";
+
+interface Playlist {
+  playlistCode: number;
+  playlistName: string;
+  createdTime: Date;
+}
+
+export interface PlaylistsState {
+  playlists: Playlist[];
+  selectedPlaylist?: Playlist;
+  totalPages: number;
+}
+
+const initialState: PlaylistsState = {
+  playlists: [],
+  totalPages: 0,
+};
+
+export const playlistsSlice = createSlice({
+  name: 'playlists',
+  initialState,
+  reducers: {
+    setPlaylists: (state, action: PayloadAction<Playlist[]>) => {
+      state.playlists = action.payload;
+    },
+    setSelectedPlaylist: (state, action: PayloadAction<Playlist>) => {
+      state.selectedPlaylist = action.payload;
+    },
+    setTotalPages: (state, action: PayloadAction<number>) => {
+      state.totalPages = action.payload;
+    },
+  },
+});
+
+export const { setPlaylists, setSelectedPlaylist, setTotalPages } = playlistsSlice.actions;
+export default playlistsSlice.reducer;
