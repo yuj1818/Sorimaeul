e76ebb3ff823789fa6e53abd673e4b10f043c9ef
탁윤희 <gwalgwal98@naver.com>
2024-03-26 00:45:58 +0900

    feat: playlist 조회(전체/상세) 기능 구현

--- Front/src/utils/playlistAPI.ts
+++ Front/src/utils/playlistAPI.ts
@@ -13,6 +13,26 @@ export const createPlaylist = (data: PlaylistCreateInterface) => {
   .catch(err => console.log(err))
 }
 
+// 플레이리스트 전체 목록 조회
+interface ListParams {
+  page: number;
+}
+
+export const getPlaylists = (page: number = 1) => {
+  const params: ListParams = { page };
+  return API.get(URL, { params })
+  .then(res => res.data)
+  .catch(err => console.log(err))
+}
+
+// 플레이리스트 상세 조회 : 플레이리스트 내 커버 목록 조회
+export const getPlaylist = async (playlistCode: string) => {
+  return API.get(URL + `/${playlistCode}`)
+  .then((res) => res.data)
+  .catch(err => console.log(err))
+}
+
+
 // 플레이리스트 삭제 
 export const deletePlaylist = (playlistCode: string) => {
   return API.delete(URL + `/${playlistCode}`)
