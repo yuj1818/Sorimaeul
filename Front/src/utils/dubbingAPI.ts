2f870230f53c35ac83b5ea6443040974d3121a45
yuj1818 <po042@naver.com>
2024-03-26 16:09:16 +0900

    feat: 더빙 원본 영상 정보 조회  api 호출 함수 구현

--- Front/src/utils/dubbingAPI.ts
+++ Front/src/utils/dubbingAPI.ts
@@ -11,3 +11,9 @@ export const getSourceVideoList = (page: number) => {
     .then(res => res.data)
     .catch(err => console.error(err))
 };
+
+export const getSourceVideo = (sourceCode: string) => {
+  return API.get(URL + `/video/${sourceCode}`)
+    .then(res => res.data)
+    .catch(err => console.error(err))
+};
