b306fee39e8c411e5a1a72454768420ed56f8210
탁윤희 <gwalgwal98@naver.com>
2024-03-27 12:08:25 +0900

    design: 메인페이지 full screen paging 구현

--- Front/src/pages/home/HomePage.tsx
+++ Front/src/pages/home/HomePage.tsx
@@ -1,13 +1,107 @@
-import React from "react";
-import Header from "../../components/common/Header";
+import React, { useEffect, useRef, useState } from 'react';
+import Header from '../../components/common/Header';
+import styled from "styled-components";
 
-// 메인페이지
-const HomePage: React.FC = () => {
-  return (
-    <>
-    <Header/>
-      <h1>메인 페이지입니다. 예쁘게 꾸미고 싶어요.</h1>
-    </>
-  );
+const Outer = styled.div`
+height: calc(100vh - 132px);
+overflow-y: auto;
+
+&::-webkit-scrollbar {
+  display: none;
 }
+`;
+
+const Page1 = styled.div`
+  height: 100vh;
+  display: flex;
+  background-color: #f7f6cf;
+`;
+
+const Page2 = styled.div`
+  height: 100vh;
+  display: flex;
+  background-color: #b6d8f2;
+`;
+
+const Page3 = styled.div`
+  height: 100vh;
+  display: flex;
+  background-color: #f4cfdf;
+`
+
+const HomePage: React.FC = () => {
+  const DIVIDER_HEIGHT = 5;
+  const outerDivRef = useRef<HTMLDivElement>(null);
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지 상태 관리
+
+  useEffect(() => {
+    const wheelHandler = (e: WheelEvent) => {
+      e.preventDefault();
+      if (!outerDivRef.current) return;
+
+      const { deltaY } = e;
+      const { scrollTop } = outerDivRef.current;
+      const pageHeight = window.innerHeight;
+
+      if (deltaY > 0) { // 스크롤 내릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setCurrentPage(2);
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          outerDivRef.current.scrollTo({
+            top: pageHeight * 2 + DIVIDER_HEIGHT * 2,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setCurrentPage(3);
+        }
+      } else { // 스크롤 올릴 때
+        if (scrollTop >= 0 && scrollTop < pageHeight) {
+          // 현재 1페이지
+        } else if (scrollTop >= pageHeight && scrollTop < pageHeight * 2) {
+          outerDivRef.current.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setCurrentPage(1);
+        } else {
+          outerDivRef.current.scrollTo({
+            top: pageHeight + DIVIDER_HEIGHT,
+            left: 0,
+            behavior: 'smooth',
+          });
+          setCurrentPage(2);
+        }
+      }
+    };
+
+    const outerDivRefCurrent = outerDivRef.current;
+    if (outerDivRefCurrent) {
+      outerDivRefCurrent.addEventListener('wheel', wheelHandler);
+    }
+    return () => {
+      if (outerDivRefCurrent) {
+        outerDivRefCurrent.removeEventListener('wheel', wheelHandler);
+      }
+    };
+  }, []);
+
+  return (
+
+    <Outer ref={outerDivRef}>
+    <Header />
+      <Page1></Page1>
+      <Page2></Page2>
+      <Page3></Page3>
+    </Outer>
+
+
+  );
+};
+
 export default HomePage;
