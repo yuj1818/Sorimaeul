5e2fa062a7f1d76be12c9b549297896b5265e79c
강주원 <wizuded@naver.com>
2024-03-27 09:26:54 +0900

    feat: 파일 확장자 검사하는 function 작성

--- Back/src/main/java/com/usagi/sorimaeul/utils/FileUtil.java
+++ Back/src/main/java/com/usagi/sorimaeul/utils/FileUtil.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 public class FileUtil {
 
@@ -19,4 +21,18 @@ public class FileUtil {
         Path path = Paths.get(filePath);
         Files.write(path, data);
     }
+
+    // 파일 확장자 추출
+    public static String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".") + 1);
+    }
+
+
+    // 허용된 확장자 여부 확인
+    public static final List<String> ALLOWED_EXTENSIONS = Arrays.asList("mp3", "m4a", "wav", "flac", "aac", "aiff");
+
+    public static boolean isAllowedExtension(String extension) {
+        return ALLOWED_EXTENSIONS.contains(extension.toLowerCase());
+    }
+
 }
