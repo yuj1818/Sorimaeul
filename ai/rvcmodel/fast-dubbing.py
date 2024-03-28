e1ba2e73cf8d1d1dcd1b05058cae49eb5fdb5ac7
김정민 <kjm1994@naver.com>
2024-03-28 17:54:28 +0900

    fix: 더빙 영상 생성 로직 S3 서버 사용

--- ai/rvcmodel/fast-dubbing.py
+++ ai/rvcmodel/fast-dubbing.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 from moviepy.editor import VideoFileClip, AudioFileClip, CompositeAudioClip
+from dotenv import load_dotenv
 
 import os, shutil
 import requests
 import logging
+
 
 logger = logging.getLogger(__name__)
 
@@ -25,60 +27,63 @@ app.add_middleware(
 cur_dir = os.getcwd()
 root_path = f"{cur_dir}/sorimaeul-data/dub"
 
+load_dotenv(os.path.join(cur_dir, ".env"))
+s3_url = os.environ["S3_URL"]
+
 
 class Request(BaseModel):
     userCode: int
     dubCode: int
-    videoSourceCode: int
-    inferredVoice: List[int]
     dubName: str
+    videoURL: str
+    voiceURL: List[str]
+    
+
+# 
+def download_file(url: str, filename: str):
+    with open(filename, 'wb') as f:
+        response = requests.get(f"{s3_url}/{url}")
+        if response.status_code == 200:
+            f.write(response.content)
+        else:
+            raise HTTPException(status_code=response.status_code, detail="Failed to download file")
 
 
 # 더빙 영상 제작
 def create_dubbing(request: Request):
     userCode = request.userCode
     dubCode = request.dubCode
-    videoSourceCode = request.videoSourceCode
-    inferredVoice = request.inferredVoice
     dubName = request.dubName
+    videoURL = request.videoURL
+    voiceURL = request.voiceURL
+
+    logger.info(f"Start create video: {root_path}/{dubCode}/{dubCode}.mp4")
 
     try:
-        video_file = f"{root_path}/source_{videoSourceCode}/origin/video/video.mp4"
-        audio_origin_path = f"{root_path}/source_{videoSourceCode}/origin/audio"
-        audio_recorded_path = f"{root_path}/source_{videoSourceCode}/user_{userCode}/Unconverted"
-        audio_inferred_path = f"{root_path}/source_{videoSourceCode}/user_{userCode}/Converted"
-
-        output_path = f"{root_path}/source_{videoSourceCode}/dub_{dubCode}/dub_{dubCode}.mp4"
-
-        logger.info(f"Start create video: {root_path}/source_{videoSourceCode}/dub_{dubCode}/dub_{dubCode}.mp4")
+        dub_path = f"{root_path}/{dubCode}"
+        video_file = f"{dub_path}/video.mp4"
+        download_file(videoURL, video_file)
 
         video = VideoFileClip(video_file)
         audio = video.audio
 
         combine_audio = [audio]
 
-        for idx, flag in enumerate(inferredVoice):
-            if idx == 0:
-                pass
-
-            if flag == 0:
-                add_audio = f"{audio_origin_path}/{idx}.wav"
-            elif flag == 1:
-                add_audio = f"{audio_recorded_path}/{idx}.wav"
-            elif flag == 2:
-                add_audio = f"{audio_inferred_path}/{idx}.wav"
-            
-            combine_audio.append(AudioFileClip(add_audio))
+        for idx, voice_url in enumerate(voiceURL):
+            voice_file = f"{dub_path}/voice_{idx}.mp3"
+            download_file(voice_url, voice_file)
+            voice = AudioFileClip(voice_file)
+            combine_audio.append(voice)
             
         audio = CompositeAudioClip(combine_audio)
 
         video = video.set_audio(audio)
 
-        video.write_videofile(output_path, codec='libx264', audio_codec='aac')
-        logger.info(f"Write video {output_path}")
+        video.write_videofile(dub_path, codec='libx264', audio_codec='aac')
+        logger.info(f"Write video {dub_path}")
 
         response = requests.post("https://j10e201.p.ssafy.io/api/dub/save",
-                                 json={"dubCode":dubCode, "path":output_path})
+                                 json={"dubCode":dubCode, "path":dub_path})
         response.raise_for_status()
         
         logger.info(f"Response status {response.status_code}")
@@ -90,6 +95,10 @@ def create_dubbing(request: Request):
     
     finally:
         sendNotification(userCode, dubCode, msg)
+
+        if os.path.exists(dub_path):
+            shutil.rmtree(dub_path)
+            logger.info(f"Remove {dub_path}")
 
 
 # 알림 전송
