2e3259adeedea8e67a694c30997308e23b886dda
탁윤희 <gwalgwal98@naver.com>
2024-03-29 05:52:03 +0900

    feat: cover 좋아요 api 연결

--- Front/src/pages/aiCover/CoverDetailPage.tsx
+++ Front/src/pages/aiCover/CoverDetailPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { deleteCover, getCover } from "../../utils/coverAPI";
+import { deleteCover, getCover, likeCover, unlikeCover } from "../../utils/coverAPI";
 import { CoverDetailInterface } from "../../components/aiCover/CoverInterface";
 import { Button } from "../../components/common/Button";
 import { useDispatch, useSelector } from "react-redux";
@@ -11,6 +11,8 @@ import { getCoverComment } from "../../utils/commentAPI";
 import { setCategory, setComments, setSelectedPostId } from "../../stores/comment";
 import ColorLine from "../../components/aiCover/ColorLine";
 import styled from "styled-components";
+import heart from "../../assets/heart.png";
+import inactiveHeart from "../../assets/inactiveHeart.png";
 
 const Container = styled.div`
   width: 75%;
@@ -131,12 +133,13 @@ const Container = styled.div`
     }
   `
 
-
 const CoverDetailPage: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const params = useParams();
   const [data, setData] = useState<CoverDetailInterface | null>(null);
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCount, setLikeCount] = useState(0);
   const loggedInUserNickname = useSelector((state: RootState) => state.user.nickname);
   const coverCode = params.id;
 
@@ -146,6 +149,9 @@ const CoverDetailPage: React.FC = () => {
         if (coverCode) {
           const data = await getCover(coverCode);
           setData(data);
+          console.log(data);
+          setIsLiked(data.liked);
+          setLikeCount(data.likeCount);
           const commentData = await getCoverComment(coverCode);
           dispatch(setCategory("cover"));
           dispatch(setSelectedPostId(coverCode));
@@ -155,11 +161,9 @@ const CoverDetailPage: React.FC = () => {
         console.error("커버 데이터를 가져오는데 실패했습니다.");
       }
     })();
-  }, [coverCode, dispatch]);
+  }, [coverCode, isLiked, dispatch]);
 
-
-
-  const handleDelete = async (e: React.MouseEvent<HTMLButtonElement> ) => {
+  const handleDelete = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
       if (coverCode) {
@@ -169,64 +173,93 @@ const CoverDetailPage: React.FC = () => {
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
+  const handleLike = async () => {
+    if (coverCode) {
+      if (isLiked) {
+        // 이미 좋아요를 누른 상태라면 좋아요 취소
+        try {
+          await unlikeCover(coverCode);
+          setIsLiked(false);
+        } catch (err) {
+          console.error("좋아요 취소 처리 중 오류가 발생했습니다.", err);
+        }
+      } else {
+        // 좋아요를 누르지 않은 상태라면 좋아요를 시도
+        try {
+          await likeCover(coverCode);
+          setIsLiked(true);
+        } catch (err) {
+          console.error("좋아요 처리 중 오류가 발생했습니다.", err);
+        }
+      }
+    };
+  };
 
   const openPlaylistAddModal = () => {
     dispatch(openModal({
       modalType: "playlistadd",
       payload: { coverCode: coverCode }
     }))
-  }
+  };
 
   return (
     <>
-    <ColorLine />
+      <ColorLine />
       {data &&
         <Container>
-        <h1 className="title"> {data.coverName} </h1>
-        <div className="content-container">
-        
-          <div className="media-section">
-            <img className="thumbnail" src={data.thumbnailPath} alt="Cover Thumbnail"/>
-            <div className="auto-player">
-              음원 저장 경로 {data.storagePath}
-            </div>
-          </div>
-          <div className="info-section">
-            <div className="info-box">
-              <div className="profile">
-                <img className="profile-image" src="" alt="Creator Profile Image" />
-                <p className="nickname"> {data.nickname} </p>
+          <h1 className="title"> {data.coverName} </h1>
+          <div className="content-container">
+
+            <div className="media-section">
+              <img className="thumbnail" src={data.thumbnailPath} alt="Cover Thumbnail" />
+              <div className="auto-player">
+                음원 저장 경로 {data.storagePath}
               </div>
-              <p className="detail-line" />
-              <p className="cover-detail"> {data.coverDetail} </p>
-              <div className="like-section">
-                <button onClick={openPlaylistAddModal}>플레이리스트에 추가</button>
-                <span>좋아요 여부 {data.isLiked} </span>
-                <span>좋아요 수 {data.likeCount}</span>
+            </div>
+            <div className="info-section">
+              <div className="info-box">
+                <div className="profile">
+                  <img className="profile-image" src="" alt="Creator Profile Image" />
+                  <p className="nickname"> {data.nickname} </p>
+                </div>
                 <p className="detail-line" />
-              </div>
-              <div className="song-info">
-                <p>노래 정보</p>
-                <p className="song-title">노래 제목: {data.title}</p>
-                <p className="original-singer">원곡 가수: {data.singer}</p>
-                <p className="cover-singer">목소리 모델: {data.coverSinger}</p>
+                <p className="cover-detail"> {data.coverDetail} </p>
+                <div className="like-section">
+                  <button onClick={openPlaylistAddModal}>플레이리스트에 추가</button>
+                  <span onClick={handleLike}>
+                    {isLiked ? (
+                      <img src={heart} alt="Active Heart" />
+                    ) : (
+                      <img src={inactiveHeart} alt="Inactive Heart" />
+                    )}
+
+                  </span>
+                  <span>좋아요 수 {data.likeCount}</span>
+                  <p className="detail-line" />
+                </div>
+                <div className="song-info">
+                  <p>노래 정보</p>
+                  <p className="song-title">노래 제목: {data.title}</p>
+                  <p className="original-singer">원곡 가수: {data.singer}</p>
+                  <p className="cover-singer">목소리 모델: {data.coverSinger}</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </Container>
-      
+        </Container>
+
       }
-      
-      
+
+
       {data && data.nickname === loggedInUserNickname &&
-        <Button onClick={() => navigate(`/cover/edit/${params.id}`) } $marginLeft={0} $marginTop={0}>수정</Button>}
+        <Button onClick={() => navigate(`/cover/edit/${params.id}`)} $marginLeft={0} $marginTop={0}>수정</Button>}
 
       {data && data.nickname === loggedInUserNickname &&
         <Button onClick={handleDelete} $marginLeft={0} $marginTop={0}>삭제</Button>}
       <CommentComponent ></CommentComponent>
-    
+
     </>
   );
 };
