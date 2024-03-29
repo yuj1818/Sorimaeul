1f8ee69bcfa41cb500973cb8ed5b81c12d34f699
김정민 <kjm1994@naver.com>
2024-03-29 11:17:10 +0900

    fix: 알림 전송 주소 수정

--- ai/rvcmodel/infer-cover.py
+++ ai/rvcmodel/infer-cover.py
@@ -100,7 +100,7 @@ def create_cover(request: Request):
 def sendNotification(userCode, targetCode, msg):
     logger.info("Send notification")
     try:
-        response = requests.post(f"https://j10e201.p.ssafy.io/api/sse/notify",
+        response = requests.post(f"https://j10e201.p.ssafy.io/api/notify/send",
                                  json={"userCode":userCode,
                                        "name":"cover",
                                        "data": {
