d88274cdd3768605bd915b590ea7902be9777f87
탁윤희 <gwalgwal98@naver.com>
2024-03-29 23:49:14 +0900

    feat: 회원 정보 수정 컴포넌트 생성

--- Front/src/pages/user/ProfilePage.tsx
+++ Front/src/pages/user/ProfilePage.tsx
@@ -5,6 +5,7 @@ import { RootState } from "../../stores/store";
 import ColorLine from "../../components/profile/ColorLine";
 import CoverBox from "../../components/profile/aiCover/CoverBox";
 import { styled } from "styled-components";
+import UserEditor from "../../components/profile/UserEditor";
 
 const Container = styled.div`
   display: flex;
@@ -15,14 +16,20 @@ const MenuBarContainer = styled.div`
   flex: 0 0 20%; 
   padding-left: 70px;
   padding-right: 70px;
+  display: flex;
+  flex-direction: column
 `;
 
 const ContentContainer = styled.div`
   flex: 0 0 80%; 
 `;
 
+
+const LearnCountContainer = styled.div``;
+
 function ProfilePage() {
   const selectedMenu = useSelector((state: RootState) => state.menu.selectedMenu);
+
 
   let ComponentToShow;
   switch (selectedMenu) {
@@ -37,8 +44,11 @@ function ProfilePage() {
   return (
     <>
     <ColorLine/>
+
     <Container>
+      
       <MenuBarContainer>
+      <UserEditor />
         <MenuBar />
       </MenuBarContainer>
       <ContentContainer>
