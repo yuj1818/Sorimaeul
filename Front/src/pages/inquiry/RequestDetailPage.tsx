21ffa1d5a1b1e978f9bd54d891f9b0a80cb8fcb1
yuj1818 <po042@naver.com>
2024-03-29 17:58:59 +0900

    feat: 요청 게시글 상세 조회 기능 구현

--- Front/src/pages/inquiry/RequestDetailPage.tsx
+++ Front/src/pages/inquiry/RequestDetailPage.tsx
@@ -3,8 +3,12 @@ import { Line } from "../../components/common/Line";
 import { Button } from "../../components/common/Button";
 import ColorLine from "../../components/inquiry/ColorLine";
 import { useNavigate, useParams } from "react-router-dom";
+import { getRequestDetail } from "../../utils/inquiryAPI";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../stores/store";
 
-const ContentBox = styled.div`
+const ContentBox = styled.div<{ $isMine?: boolean }>`
   display: flex;
   flex-direction: column;
   gap: .75rem;
@@ -34,35 +38,51 @@ const ContentBox = styled.div`
     margin-left: auto;
     display: flex;
     gap: .5rem;
+    .mine {
+      display: ${(props) => props.$isMine ? 'block' : 'none'};
+    }
   }
 `
+
+export interface InfoData {
+  title: string;
+  content: string;
+  createdTime: string;
+  nickname: string;
+}
 
 function RequestDetailPage() {
   const navigate = useNavigate();
   const params =  useParams();
+  const userName = useSelector((state: RootState) => state.user.nickname);
+  const [data, setData] = useState<InfoData>();
 
-  const data = {
-    id: 2,
-    title: "짱구는 못말려 영상도 올려주세요",
-    content: "짱구는 못말려 영상도 재미있던데 이거도 올려주세요!!\n\nhttps://www.youtube.com/watch?v=4QUgbmFUe_M&ab_channel=%EC%A7%B1%EA%B5%AC%ED%95%98%EC%9D%B4%EB%9D%BC%EC%9D%B4%ED%8A%B8",
-    createdTime: "2024-03-19"
-  }
+  const getDetail = async () => {
+    if (params.id) {
+      const res = await getRequestDetail(params.id);
+      setData(res);
+    }
+  };
+
+  useEffect(() => {
+    getDetail();
+  }, [])
 
   return (
     <>
       <ColorLine />
-      <ContentBox>
-        <h2 className="title">짱구는 못말려 영상도 올려주세요</h2>
+      <ContentBox $isMine={data?.nickname === userName}>
+        <h2 className="title">{data?.title}</h2>
         <Line />
         <div className="mb-4 flex flex-col gap-4">
-          <p className="date">{data.createdTime}</p>
-          <div className="content">{data.content}</div>
+          <p className="date">{data?.createdTime}</p>
+          <div className="content">{data?.content}</div>
         </div>
         <Line />
         <div className="button-box">
           <Button onClick={() => navigate('/request')} $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>목록보기</Button>
-          <Button onClick={() => navigate(`/request/${params.id}/edit`)} $marginLeft={0} $marginTop={0} $width={4.375} $height={2} $background="#28BEFF">수정</Button>
-          <Button $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>삭제</Button>
+          <Button className="mine" onClick={() => navigate(`/request/${params.id}/edit`)} $marginLeft={0} $marginTop={0} $width={4.375} $height={2} $background="#28BEFF">수정</Button>
+          <Button className="mine" $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>삭제</Button>
         </div>
       </ContentBox>
     </>
