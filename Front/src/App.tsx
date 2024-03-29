b140f01f6f484b9b56910562ca5140364aeb8c46
탁윤희 <gwalgwal98@naver.com>
2024-03-29 16:47:46 +0900

    remove: 사용하지 않는 페이지(커버 수정) 제거

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -16,7 +16,6 @@ import SideBar from './components/common/SideBar';
 import ModelCreatePage from './pages/voiceModel/ModelCreatePage';
 import ModelDetailPage from './pages/voiceModel/ModelDetailPage';
 import RecordingPage from './pages/voiceModel/RecordingPage';
-import CoverUpdatePage from './pages/aiCover/CoverUpdatePage';
 import CoverDetailPage from './pages/aiCover/CoverDetailPage';
 import CoverListPage from './pages/aiCover/CoverListPage';
 import CoverResultPage from './pages/aiCover/CoverResultPage';
@@ -143,10 +142,6 @@ const router = createBrowserRouter([
               {
                 path: "register/:id",
                 element: <CoverResultPage />
-              },
-              {
-                path: "edit/:id",
-                element: <CoverUpdatePage />
               },
               {
                 path: "board/:id",
