21f8872b0f1de5a51da2d6f91dab61737dce5a37
yuj1818 <po042@naver.com>
2024-03-30 03:25:52 +0900

    feat: 삭제 기능 구현

--- Front/src/pages/inquiry/RequestDetailPage.tsx
+++ Front/src/pages/inquiry/RequestDetailPage.tsx
@@ -3,12 +3,12 @@ import { Line } from "../../components/common/Line";
 import { Button } from "../../components/common/Button";
 import ColorLine from "../../components/inquiry/ColorLine";
 import { useNavigate, useParams } from "react-router-dom";
-import { getRequestDetail } from "../../utils/inquiryAPI";
+import { getRequestDetail, deleteRequest } from "../../utils/inquiryAPI";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../stores/store";
 
-const ContentBox = styled.div<{ $isMine?: boolean }>`
+const ContentBox = styled.div`
   display: flex;
   flex-direction: column;
   gap: .75rem;
@@ -38,9 +38,6 @@ const ContentBox = styled.div<{ $isMine?: boolean }>`
     margin-left: auto;
     display: flex;
     gap: .5rem;
-    .mine {
-      display: ${(props) => props.$isMine ? 'block' : 'none'};
-    }
   }
 `
 
@@ -55,7 +52,7 @@ function RequestDetailPage() {
   const navigate = useNavigate();
   const params =  useParams();
   const userName = useSelector((state: RootState) => state.user.nickname);
-  const [data, setData] = useState<InfoData>();
+  const [data, setData] = useState<InfoData | null>(null);
 
   const getDetail = async () => {
     if (params.id) {
@@ -66,25 +63,40 @@ function RequestDetailPage() {
 
   useEffect(() => {
     getDetail();
-  }, [])
+  }, [params.id])
+
+  const clickDelete = async () => {
+    if (params.id) {
+      await deleteRequest(params.id);
+      navigate('/request');
+    }
+  };
 
   return (
     <>
       <ColorLine />
-      <ContentBox $isMine={data?.nickname === userName}>
-        <h2 className="title">{data?.title}</h2>
-        <Line />
-        <div className="mb-4 flex flex-col gap-4">
-          <p className="date">{data?.createdTime}</p>
-          <div className="content">{data?.content}</div>
-        </div>
-        <Line />
-        <div className="button-box">
-          <Button onClick={() => navigate('/request')} $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>목록보기</Button>
-          <Button className="mine" onClick={() => navigate(`/request/${params.id}/edit`)} $marginLeft={0} $marginTop={0} $width={4.375} $height={2} $background="#28BEFF">수정</Button>
-          <Button className="mine" $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>삭제</Button>
-        </div>
-      </ContentBox>
+      {
+        data &&
+        <ContentBox>
+          <h2 className="title">{data.title}</h2>
+          <Line />
+          <div className="mb-4 flex flex-col gap-4">
+            <p className="date">{data.createdTime}</p>
+            <div className="content">{data.content}</div>
+          </div>
+          <Line />
+          <div className="button-box">
+            <Button onClick={() => navigate('/request')} $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>목록보기</Button>
+            {
+              data.nickname === userName &&
+              <>
+                <Button className="mine" onClick={() => navigate(`/request/${params.id}/edit`)} $marginLeft={0} $marginTop={0} $width={4.375} $height={2} $background="#28BEFF">수정</Button>
+                <Button className="mine" onClick={clickDelete} $marginLeft={0} $marginTop={0} $width={4.375} $height={2}>삭제</Button>
+              </>
+            }
+          </div>
+        </ContentBox>
+      }
     </>
   )
 }
