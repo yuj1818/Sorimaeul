5d820dd1c06ff88f15d4d424b76719ebcf790134
탁윤희 <gwalgwal98@naver.com>
2024-03-31 02:22:17 +0900

    feat: playlist header modal 생성

--- Front/src/components/common/playlist/header/Playlist.tsx
+++ Front/src/components/common/playlist/header/Playlist.tsx
@@ -1,32 +1,48 @@
 import styled from 'styled-components';
+import playlists from "../../../../assets/playlistCheck.png";
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../../../stores/modal';
 
 const PlaylistComponent = styled.div`
   box-sizing: border-box;
   border: 1px dashed #000000;
   border-radius: 50px;
   padding: 20px; 
-  margin: 10px; 
+  margin: 20px; 
   margin-left: auto;
-  width: 300px; 
-  height: 50px; 
+  width: 450px; 
+  height: 55px; 
   right: 0;
+
+  .list-icon {
+    margin-bottom: 10px;
+  }
 `;
 
 
 const Playlist: React.FC = () => {
-  // 더미 데이터
-  const songs = [
-    { title: "노래 제목 1", artist: "가수명 1" },
+  const dispatch = useDispatch();
 
-  ];
+  const openPlaylistHeaderModal = () => {
+    dispatch(openModal({
+      modalType: "playlistheader",
+    }));
+  };
+
+  // 더미 데이터
+  // const songs = [
+  //   { title: "노래 제목 1", artist: "가수명 1" },
+
+  // ];
 
   return (
     <PlaylistComponent>
-      {songs.map((song, index) => (
+      <img className="list-icon" onClick={openPlaylistHeaderModal} src={playlists} alt="Show Playlists Icon" />
+      {/* {songs.map((song, index) => (
         <div key={index}>
           <strong>{song.title}</strong> - {song.artist}
         </div>
-      ))}
+      ))} */}
     </PlaylistComponent>
   );
 };
---
+++ Front/src/components/common/playlist/modal/PlaylistHeaderModal.tsx
@@ -0,0 +1,13 @@
+import { Content } from "../../ModalStyles";
+
+const PlaylistHeaderModal: React.FC = () => {
+  return (
+    <>
+    <Content $width={40} $height={40} $borderRadius={30} $background="black">
+
+    </Content>
+    </>
+  )
+}
+
+export default PlaylistHeaderModal;
