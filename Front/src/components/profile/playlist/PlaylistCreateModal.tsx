b8f8c7180134f948c51477f5447fad58a302d9d4
탁윤희 <gwalgwal98@naver.com>
2024-03-30 12:49:28 +0900

    design: playlist 생성 모달 css 수정

--- Front/src/components/profile/playlist/PlaylistCreateModal.tsx
+++ Front/src/components/profile/playlist/PlaylistCreateModal.tsx
@@ -7,15 +7,37 @@ import { PlaylistCreateInterface, createPlaylist } from "../../../utils/playlist
 import { useDispatch, useSelector } from "react-redux";
 import { closeModal } from "../../../stores/modal";
 import { addPlaylist } from "../../../stores/playlists";
+import logo from "../../../assets/sideSmLogo.png";
+
+const ModalHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LabelContainer = styled.div`
+  display: flex;
+  align-items: center; 
+`;
 
 const Label = styled.label`
-  font-style: normal;
+  margin-left: 10px;
   font-weight: 500;
-  font-size: 13px;
+  font-size: 20px;
   line-height: 15px;
   color: #888888;
 `;
 
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const StyledInputBox = styled(InputBox)`
+  width: 350px;
+  margin-left: 45px;
+  margin-right: 20px;
+`;
 
 
 function PlaylistCreateModal () {
@@ -45,11 +67,18 @@ function PlaylistCreateModal () {
 
   return (
     <>
-    <Content>
+    <Content $height={9}>
+      <ModalHeader>
+      <LabelContainer>
+      <img src={logo} className="mb-1 w-10" />
       <Label htmlFor="playlistName">플레이리스트 이름을 입력해주세요.</Label>
-      <InputBox type="text" id="playlistName" name="playlistName" value={data.playlistName} onChange={ handlePlaylistName}/>
-      <Button onClick={submitHandler} $marginLeft={0} $marginTop={0} disabled={playlistName.trim().length === 0}>생성</Button>
+      </LabelContainer>
       <CloseButton onClick={() => dispatch(closeModal())}>x</CloseButton>
+      </ModalHeader>
+      <InputContainer>
+      <StyledInputBox type="text" id="playlistName" name="playlistName" value={data.playlistName} onChange={ handlePlaylistName}/>
+      <Button onClick={submitHandler} $marginLeft={0} $marginTop={0} disabled={playlistName.trim().length === 0}>생성</Button>
+      </InputContainer>
     </Content>
     </>
   )
