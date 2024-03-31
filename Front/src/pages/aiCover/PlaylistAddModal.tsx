a0a117bb9bf8c2e9ad9ad55141f7f359f0898feb
탁윤희 <gwalgwal98@naver.com>
2024-03-31 19:50:05 +0900

    refactor: playlist 선택 컴포넌트에서 플리에 커버 추가 기능 분리

--- Front/src/pages/aiCover/PlaylistAddModal.tsx
+++ Front/src/pages/aiCover/PlaylistAddModal.tsx
@@ -3,6 +3,8 @@ import { Content } from "../../components/common/ModalStyles";
 import { useDispatch, useSelector } from "react-redux";
 import { closeModal } from '../../stores/modal';
 import PlaylistSelect from '../../components/common/playlist/modal/PlaylistSelect';
+import { RootState } from '../../stores/store';
+import { addCoverToList } from '../../utils/playlistAPI';
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -29,6 +31,22 @@ const Title = styled.div`
 
 function PlaylistAddModal() {
   const dispatch = useDispatch();
+  const coverCode = useSelector((state:RootState) => state.modal.payload?.coverCode);
+
+  const handlePlaylistSelect = async (playlistCode: string) => {
+    try {
+      // addCoverToList API를 호출하여 커버를 플레이리스트에 추가합니다.
+      const res = await addCoverToList(playlistCode, coverCode);
+      if (res.status === 201) {
+        alert('플레이리스트에 커버를 추가했습니다!');
+        dispatch(closeModal()); // 성공 시 모달을 닫습니다.
+      } else {
+        alert('이미 커버가 플레이리스트에 담겨있습니다.');
+      }
+    } catch (error) {
+      console.error('플레이리스트에 커버를 추가하는 중 오류가 발생했습니다.', error);
+    }
+  };
 
   return (
     <>
@@ -37,7 +55,7 @@ function PlaylistAddModal() {
           <Title>플레이리스트를 선택해주세요.</Title>
           <CloseButton color="#FDA06C" onClick={() => dispatch(closeModal())}>x</CloseButton>
         </HeaderContainer>
-        <PlaylistSelect />
+        <PlaylistSelect onPlaylistSelect={handlePlaylistSelect}/>
       </Content>
     </>
   )
