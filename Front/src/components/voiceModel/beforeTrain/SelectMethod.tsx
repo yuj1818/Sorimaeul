eb9194d03a45135227a7bc0d567b68b911bcb399
yuj1818 <po042@naver.com>
2024-04-01 06:16:13 +0900

    design: 잔여 학습 횟수 디스플레이

--- Front/src/components/voiceModel/beforeTrain/SelectMethod.tsx
+++ Front/src/components/voiceModel/beforeTrain/SelectMethod.tsx
@@ -20,10 +20,20 @@ const Container = styled.div`
   .line {
     border-color: #AC69FF;
   }
+
+  .learnCnt {
+    text-align: right;
+    padding-right: 1rem;
+    color: white;
+    .num {
+      color: #AC69FF;
+    }
+  }
 `
 
 function SelectMethod() {
   const method = useSelector((state: RootState) => state.voiceModel.method);
+  const learnCount = useSelector((state: RootState) => state.user.learnCount);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -46,6 +56,7 @@ function SelectMethod() {
         method === "model" &&
         <UploadModelMethod />
       }
+      <p className="learnCnt">남은 학습 횟수: <span className="num">{learnCount}</span>회</p>
     </Container>  
   )
 }
