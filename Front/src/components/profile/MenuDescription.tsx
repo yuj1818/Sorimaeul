e247fa3d8094ce617f6dc314f9e827259f147815
yuj1818 <po042@naver.com>
2024-04-01 07:20:03 +0900

    feat: 나의 음성 모델 리스트 컴포넌트 구현

--- Front/src/components/profile/MenuDescription.tsx
+++ Front/src/components/profile/MenuDescription.tsx
@@ -9,21 +9,21 @@ const MenuHeader = styled.div`
 `;
 
 const BigText = styled.span`
-  font-size: 70px;
+  font-size: 3.125rem;
   font-family: 'GmarketSansBold';
 `;
 
 const MiddleText = styled.span`
-  font-size: 55px;
+  font-size: 2.5rem;
   font-family: 'GmarketSansBold';
   margin-top: 9px;
-  margin-right: 45px;
+  margin-right: 1rem;
 `;
 
 const SmallText = styled.span`
   margin-top: 12px;
-  font-size: 40px;
-  font-family: GmarketSansLight;
+  font-size: 1.25rem;
+  font-family: 'GmarketSansLight';
 `;
 
 interface Props {
