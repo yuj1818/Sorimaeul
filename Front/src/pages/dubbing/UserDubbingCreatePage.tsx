914a57b80c1206f306be5fb2b2749b0758cd66a4
yuj1818 <po042@naver.com>
2024-04-02 15:24:22 +0900

    feat: 더빙 제작 페이지 css 수정

--- Front/src/pages/dubbing/UserDubbingCreatePage.tsx
+++ Front/src/pages/dubbing/UserDubbingCreatePage.tsx
@@ -1,8 +1,10 @@
+import ColorLine from "../../components/dubbing/ColorLine";
 import Dubbing from "../../components/dubbing/contentCreation/Dubbing";
 
 function UserDubbingCreatePage() {
   return (
     <>
+      <ColorLine />
       <Dubbing />
     </>
   )
