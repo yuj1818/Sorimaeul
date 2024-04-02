bfa5c108cdd9b5b8cf3c3628026e9081a7c70bf2
yuj1818 <po042@naver.com>
2024-04-02 15:23:01 +0900

    feat: svg 관련 라이브러리 설치

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import svgr from '@svgr/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr()
+  ],
   server: {
     port: 3000
   }
