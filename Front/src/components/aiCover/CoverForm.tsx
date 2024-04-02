cdca7b106f894cb2cde86b16717c9081e9e149f7
탁윤희 <gwalgwal98@naver.com>
2024-04-02 09:41:36 +0900

    design: 피치 조절 설명 문구 수정

--- Front/src/components/aiCover/CoverForm.tsx
+++ Front/src/components/aiCover/CoverForm.tsx
@@ -273,7 +273,7 @@ const CoverForm: React.FC<Props> = ({ onSubmit }) => {
         <StepContainer>
         <label className="text-stone-500" htmlFor="pitch">피치 조절 
         </label>
-        <Tooltip message={"피치 조절에 대한 설명이 들어갑니다.두 줄이 되면 어떻게 나올지 궁금합니다."}>
+        <Tooltip message={"남성과 여성은 일반적으로 4 ~ 6키가 차이납니다. 예를 들어 남성 음성으로 여성곡을 커버하시려는 경우,  -4 ~ 6사이의 피치 조절을 추천드립니다."}>
         <HelpIcon src={help} />
         </Tooltip>
         
