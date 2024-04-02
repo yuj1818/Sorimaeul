dcf744908ef29126d3f174fd56dc1a6f92149ea5
yuj1818 <po042@naver.com>
2024-04-02 15:23:30 +0900

    design: 더빙 제작 페이지 css

--- Front/src/components/dubbing/contentCreation/Dubbing.tsx
+++ Front/src/components/dubbing/contentCreation/Dubbing.tsx
@@ -4,15 +4,133 @@ import styled from "styled-components";
 import { getOriginVoices, uploadRecord, convertRecord, createDubbing } from "../../../utils/dubbingAPI";
 import { getVoiceModels } from "../../../utils/voiceModelAPI";
 import { s3URL } from "../../../utils/s3";
-import startRecordBtn from "../../../assets/startRecordBtn.png";
-import stopRecordBtn from "../../../assets/stopRecordBtn.png";
-import reRecordBtn from "../../../assets/reRecordBtn.png";
-import playRecordBtn from "../../../assets/playRecordBtn.png";
 import Select from "react-select";
 import { Button } from "../../common/Button";
+import { ReactComponent as StartDubBtn } from "../../../assets/startDubBtn.svg";
+import { ReactComponent as StopDubBtn } from "../../../assets/stopDubBtn.svg";
+import { ReactComponent as PlayDubBtn } from "../../../assets/playDubBtn.svg";
+import { ReactComponent as ReDubBtn } from "../../../assets/reDubBtn.svg";
 
 const Container = styled.div`
   width: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  margin: 2rem auto;
+`
+
+const InputBox = styled.div`
+  display: flex;
+  height: 2.25rem;
+  align-items: center;
+  gap: .5rem;
+  
+  .title {
+    font-size: 2rem;
+    font-family: 'GmarketSansBold';
+    padding-top: .25rem;
+  }
+  
+  .title-input {
+    flex-grow: 1;
+    height: 100;
+    font-size: 1.25rem;
+    font-family: 'GmarketSansLight';
+    padding: 0 .5rem;
+    border: 1px solid #D9D9D9;
+    border-radius: 5px;
+  }
+`
+
+const VideoBox = styled.div`
+  width: 100%;
+  border-radius: 5px;
+  overflow: hidden;
+  video {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const DubbingBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: .5rem;
+
+  .character-section {
+    width: 10.6%;
+  }
+
+  .record-section {
+    width: 17%;
+  }
+
+  .model-section {
+    width: 26%;
+  }
+
+  .pitch-section {
+    width: 13.6%;
+  }
+
+  .check-section {
+    width: 21.2%;
+  }
+
+  .convert-section {
+    flex-grow: 1;
+  }
+
+  .type {
+    font-size: 1.25rem;
+    color: #727272;
+    text-align: center;
+    padding-top: .25rem;
+  }
+
+  .character {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    .box {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .name {
+        font-size: 1.25rem;
+        padding-top: .25rem;
+      }
+    }
+  }
+
+  .control-box {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    position: relative;
+    .pitch {
+      height: 65%;
+      width: 70%;
+      border: 1px solid #d9d9d9;
+      border-radius: 5px;
+      padding-left: .5rem;
+      text-align: center;
+      padding-top: .25rem;
+    }
+    .convert-btn {
+      height: 65%;
+      width: 70%;
+    }
+  }
+
+  .check-box {
+    position: absolute;
+    right: 0;
+    height: 100%;
+    width: 21.2%;
+  }
 `
 
 interface VoiceData {
@@ -43,10 +161,11 @@ function Dubbing() {
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
   const [audioBlob, setAudioBlob] = useState<Blob[]>([]);
   const [isPlay, setIsPlay] = useState<boolean[]>([]);
-  const [isConverted, setIsConverted] = useState<boolean[]>([]);
+  const [isConverted, setIsConverted] = useState<boolean>(false);
   const [model, setModel] = useState<number[]>([]);
   const [pitch, setPitch] = useState<number[]>([]);
   const [originVoicePaths, setOriginVoicePaths] = useState<string[]>([]);
+  const [convertList, setConvertList] = useState<boolean[]>([]);
   const [isPlayAll, setIsPlayAll] = useState(false);
   const audioRefs = useRef<HTMLAudioElement[]>([]);
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -70,7 +189,7 @@ function Dubbing() {
       setAudioURL(Array.from({length: res.voiceSources.length}, () => ''));
       setAudioBlob(Array.from({length: res.voiceSources.length}, () => new Blob()));
       setIsPlay(Array.from({length: res.voiceSources.length}, () => false));
-      setIsConverted(Array.from({length: res.voiceSources.length}, () => false));
+      setConvertList(Array.from({length: res.voiceSources.length}, () => false));
       audioRefs.current = res.voiceSources.map(() => new Audio());
       mediaRefs.current = res.voiceSources.map(() => new Audio());
     }
@@ -143,13 +262,13 @@ function Dubbing() {
 
   const stopRecording = (idx: number) => {
     if (mediaRecorder) {
-      mediaRecorder.stop();
-      stopAll();
       setRecordState(prev => {
         const newState = [...prev];
         newState[idx] = 2;
         return newState;
       });
+      mediaRecorder.stop();
+      stopAll();
 
       mediaRecorder.ondataavailable = (e) => {
         if (e.data.size > 0) {
@@ -225,33 +344,43 @@ function Dubbing() {
   };
 
   const convertAudio = async (idx: number) => {
-    setIsConverted(prev => {
-      const newState = [...prev];
-      newState[idx] = false;
-      return newState;
-    });
-
-    if (audioBlob[idx]) {
-      const formData = new FormData();
-      formData.append("recordFile", audioBlob[idx], `dubbing${params.sourceCode}_voice${idx + 1}.wav`);
-      if (params.sourceCode) {
-        const uploadRes = await uploadRecord(params.sourceCode, idx + 1, formData);
-        const convertRes = await convertRecord(idx + 1, {
-          videoSourceCode: parseInt(params.sourceCode),
-          modelCode: model[idx],
-          voicePath: uploadRes.voicePath,
-          pitch: pitch[idx]
-        });
-        setIsConverted(prev => {
-          const newState = [...prev];
-          newState[idx] = true;
-          return newState;
-        });
-        setVoicePaths(prev => {
-          const newState = [...prev];
-          newState[idx] = convertRes.voicePath;
-          return newState;
-        });
+    if (convertList[idx]) {
+      setConvertList(prev => {
+        const newState = [...prev];
+        newState[idx] = false;
+        return newState;
+      });
+      setVoicePaths(prev => {
+        const newState = [...prev];
+        newState[idx] = originVoicePaths[idx];
+        return newState;
+      });
+    } else {
+      setIsConverted(false);
+      setConvertList(prev => {
+        const newState = [...prev];
+        newState[idx] = true;
+        return newState;
+      });
+  
+      if (audioBlob[idx]) {
+        const formData = new FormData();
+        formData.append("recordFile", audioBlob[idx], `dubbing${params.sourceCode}_voice${idx + 1}.wav`);
+        if (params.sourceCode) {
+          const uploadRes = await uploadRecord(params.sourceCode, idx + 1, formData);
+          const convertRes = await convertRecord(idx + 1, {
+            videoSourceCode: parseInt(params.sourceCode),
+            modelCode: model[idx],
+            voicePath: uploadRes.voicePath,
+            pitch: pitch[idx]
+          });
+          setIsConverted(true);
+          setVoicePaths(prev => {
+            const newState = [...prev];
+            newState[idx] = convertRes.voicePath;
+            return newState;
+          });
+        }
       }
     }
   };
@@ -295,90 +424,135 @@ function Dubbing() {
     }
   }
 
+  useEffect(() => {
+    console.log(model)
+  }, [model])
+
   return (
     <Container>
-      <div>
-        <label htmlFor="title">제목:</label>
-        <input onChange={handleTitle} type="text" name="title" id="title" maxLength={40} placeholder="최대 40글자까지 작성 가능합니다" />
-      </div>
-      <div className="video-box">
+      <InputBox>
+        <label className="title" htmlFor="title">제목:</label>
+        <input className="title-input" onChange={handleTitle} type="text" name="title" id="title" maxLength={40} placeholder="최대 40글자까지 작성 가능합니다" />
+      </InputBox>
+      <VideoBox>
         <video ref={videoRef} src={s3URL + `${videoPath}`} />
-      </div>
-      <div>
-        <div className="flex gap-4">
-          <p>캐릭터</p>
-          <p>더빙</p>
-          <p>음성 모델</p>
-          <p>피치 조절</p>
-          <p>목소리 확인</p>
+      </VideoBox>
+      <DubbingBox>
+        <div className="flex justify-between">
+          <p className="type character-section">캐릭터</p>
+          <p className="type record-section">더빙</p>
+          <p className="type model-section">음성 모델</p>
+          <p className="type pitch-section">피치 조절</p>
+          <div className="convert-section"></div>
+          <p className="type check-section">목소리 확인</p>
         </div>
         <hr />
-        <div>
+        <div className="control-box">
           {
             originVoiceList.map((el, idx) => (
-              <div key={el.voiceIndex} className="flex">
-                <p className="character">{el.voiceName}</p>
-                {
-                  recordState[idx] === 0 &&
-                  <img onClick={() => startRecording(idx)} className="recordBtn" src={startRecordBtn} alt="startRecordBtn" />
-                }
-                {
-                  recordState[idx] === 1 &&
-                  <img onClick={() => stopRecording(idx)} className="recordBtn" src={stopRecordBtn} alt="stopRecordBtn" />
-                }
-                {
-                  recordState[idx] === 2 &&
-                  <>
-                    {
-                      isPlay[idx] ?
-                      <img onClick={() => stopAudio(idx)} src={stopRecordBtn} className="recordBtn" alt="stopPlaying" />
-                      :
-                      <img onClick={() => playAudio(idx)} className="recordBtn" src={playRecordBtn} alt="playRecordBtn" />
-                    }
-                  </>
-                }
-                {
-                  recordState[idx] === 2 && <img onClick={() => resetRecording(idx)} src={reRecordBtn} alt="" />
-                }
-                <Select 
-                  value = {
-                    modelInput.find((option) => option.value === model[idx])
+              <div key={el.voiceIndex} className="character">
+                <div className="character-section box">
+                  <p className="name">{el.voiceName}</p>
+                </div>
+                <div className="record-section flex justify-center items-center gap-2">
+                  {
+                    recordState[idx] === 0 &&
+                    <StartDubBtn fill="black" onClick={() => startRecording(idx)} className="recordBtn" />
                   }
-                  options={modelInput}
-                  onChange={(selectedOption) => setModel(prev => {
-                    const newState = [...prev];
-                    newState[idx] = selectedOption?.value || 0;
-                    return newState;
-                  })}
-                  placeholder="모델을 선택하세요" 
-                />
-                <input 
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setPitch(prev => {
+                  {
+                    recordState[idx] === 1 &&
+                    <StopDubBtn fill="black" onClick={() => stopRecording(idx)} className="recordBtn" />
+                  }
+                  {
+                    recordState[idx] === 2 &&
+                    <>
+                      {
+                        isPlay[idx] ?
+                        <StopDubBtn fill="black" onClick={() => stopAudio(idx)} className="recordBtn" />
+                        :
+                        <PlayDubBtn fill="black" onClick={() => playAudio(idx)} className="recordBtn" />
+                      }
+                    </>
+                  }
+                  {
+                    recordState[idx] === 2 && <ReDubBtn fill="black" onClick={() => resetRecording(idx)} />
+                  }
+                </div>
+                <div className="model-section flex justify-center items-center">
+                  <Select 
+                    value = {
+                      modelInput.find((option) => option.value === model[idx])
+                    }
+                    options={modelInput}
+                    onChange={(selectedOption) => setModel(prev => {
                       const newState = [...prev];
-                      newState[idx] = parseInt(e.target.value);
-                      return newState
-                    })
-                  }}
-                  className="border" 
-                  type="number" 
-                  min={-12} 
-                  max={12} 
-                  defaultValue={0} 
-                />
-                <Button disabled={model[idx] === 0 && audioURL[idx] === ""} onClick={() => convertAudio(idx)} $marginLeft={0} $marginTop={0}>변환</Button>
-                {
-                  isConverted[idx] && (isPlayAll ?
-                  <img onClick={stopAll} src={stopRecordBtn} alt="" />
-                  :
-                  <img onClick={playAll} src={playRecordBtn} alt="" />)
-                }
+                      newState[idx] = selectedOption?.value || 0;
+                      return newState;
+                    })}
+                    placeholder="모델을 선택하세요" 
+                    styles={{
+                      control: (provided) => ({
+                        ...provided,
+                        border: "1px solid #d9d9d9",
+                        borderRadius: "5px",
+                        width: "100%",
+                        height: "65%"
+                      }),
+                      option: (provided) => ({
+                        ...provided,
+                        fontSize: ".8rem"
+                      })
+                    }}
+                  />
+                </div>
+                <div className="pitch-section flex justify-center items-center">
+                  <input 
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      setPitch(prev => {
+                        const newState = [...prev];
+                        newState[idx] = parseInt(e.target.value);
+                        return newState
+                      })
+                    }}
+                    className="pitch" 
+                    type="number" 
+                    min={-12} 
+                    max={12} 
+                    defaultValue={0} 
+                  />
+                </div>
+                <div className="convert-section flex justify-center items-center">
+                  <Button 
+                    className="convert-btn"
+                    disabled={model[idx] === 0 || audioURL[idx] === ""} 
+                    onClick={() => convertAudio(idx)} 
+                    $marginLeft={0} 
+                    $marginTop={0}
+                    $background="#C9F647"
+                    $color="black"
+                  >
+                    {
+                      convertList[idx] ? '초기화' : '변환'
+                    }
+                  </Button>
+                </div>
+                <div className="check-section"></div>
               </div>
             ))
           }
+          <div className="check-box flex justify-center items-center">
+            {
+              isConverted && (isPlayAll ?
+              <StopDubBtn onClick={stopAll} fill="black" />
+              :
+              <PlayDubBtn onClick={playAll} fill="black" />)
+            }
+          </div>
         </div>
-        <Button onClick={createDubbingContents} disabled={title === ''} $marginLeft={0} $marginTop={0}>제작</Button>
-      </div>
+        <div className="w-full flex justify-center items-center">
+          <Button onClick={createDubbingContents} disabled={title === ''} $marginLeft={0} $marginTop={1} $width={5.25} $height={2} $color="#C9F647">제작</Button>
+        </div>
+      </DubbingBox>
     </Container>  
   )
 }
