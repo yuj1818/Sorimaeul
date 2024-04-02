ad770608bb285ecec52ddb3b3465a28ae7f2c776
yuj1818 <po042@naver.com>
2024-04-03 02:37:22 +0900

    refactor: svg 컴포넌트로 수정

--- Front/src/components/profile/dubbing/DubbingCard.tsx
+++ Front/src/components/profile/dubbing/DubbingCard.tsx
@@ -4,6 +4,7 @@ import { Button } from "../../common/Button";
 import { s3URL } from "../../../utils/s3";
 import { useNavigate } from "react-router-dom";
 import { deleteDubbing } from "../../../utils/dubbingAPI";
+import { ReactComponent as Icon } from "../../../assets/public.svg";
 
 const Container = styled.div`
   padding: 1rem;
@@ -87,10 +88,8 @@ const DubbingCard: React.FC<{ data: DubbingData, setDubbingContents: React.Dispa
         </div>
       </div>
       <div className="state">
-        <svg width={"25%"} height={"100%"} viewBox="0 0 42 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M21 0C11.4545 0 3.30273 6.01267 0 14.5C3.30273 22.9873 11.4545 29 21 29C30.5455 29 38.6973 22.9873 42 14.5C38.6973 6.01267 30.5455 0 21 0ZM21 24.1667C15.7309 24.1667 11.4545 19.836 11.4545 14.5C11.4545 9.164 15.7309 4.83333 21 4.83333C26.2691 4.83333 30.5455 9.164 30.5455 14.5C30.5455 19.836 26.2691 24.1667 21 24.1667ZM21 8.7C17.8309 8.7 15.2727 11.2907 15.2727 14.5C15.2727 17.7093 17.8309 20.3 21 20.3C24.1691 20.3 26.7273 17.7093 26.7273 14.5C26.7273 11.2907 24.1691 8.7 21 8.7Z" fill="#C9F647"/>
-        </svg>
-        <p className="sm-font">공개</p>
+        <Icon width="25%" height="100%" fill="#C9F647" />
+        <p className="sm-font">{data.isPublic ? '공개' : '비공개'}</p>
       </div>
       <p className="date sm-font">{data.createdTime.split('T')[0]}</p>
       <div className="button">
