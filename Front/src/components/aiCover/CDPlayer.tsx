836f27e4d773224fdef94cf0db1900da5c77e717
탁윤희 <gwalgwal98@naver.com>
2024-04-03 19:32:02 +0900

    design: cover 목록 조회 찌그러진 cd 수정

--- Front/src/components/aiCover/CDPlayer.tsx
+++ Front/src/components/aiCover/CDPlayer.tsx
@@ -32,11 +32,12 @@ const ThumbnailContainer = styled.div`
   border-radius: 50%;
   overflow: hidden;
   object-fit: cover;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const ThumbnailImage = styled.img`
   width: 100%;
-
+  height: 100%;
   border-radius: 50%;
   z-index: 1;
 `;
